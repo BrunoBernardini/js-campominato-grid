d62e15cc48d5cf9a63a84804ae6b01d7535e244b
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-21 23:03:58 +0200

    Funzione per i numeri random unici + Selezione quantità celle.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,10 +7,47 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
 
 const playBtn = document.getElementById("play");
-const diffSel = document.getElementById("difficulty");
+const numberList = [];
 
 playBtn.addEventListener("click", init);
 
 function init(){
   
+}
+
+function getSquaresQuantity(){
+  const diffSel = document.getElementById("difficulty").value;
+  let quantity;
+  switch(diffSel){
+    case "1":
+      quantity = 100;
+      break;
+    case "2":
+      quantity = 81;
+      break;
+    case "3":
+      quantity = 49;
+      break;
+    default:
+      quantity = 0;
+      break;
+  }
+  return quantity;
+}
+
+function getUniqueRandomNumber(min, max){
+  let number = null;
+  let valid = false;
+  while(!valid){
+    number = getRandomNumber(min, max);
+    if(!numberList.includes(number)){
+      valid = true;
+      numberList.push(number);
+    }
+  }
+  return number;
+}
+
+function getRandomNumber(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
