521f18074d03064caff63d92a835ce463180f3f6
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-22 00:23:52 +0200

    Prima versione funzionante.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,15 +7,68 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
 
 const playBtn = document.getElementById("play");
+const field = document.querySelector(".field");
 const numberList = [];
 
 playBtn.addEventListener("click", init);
 
 function init(){
-  
+  const cellsQuantity = getCellsQuantity();
+  if(!cellsQuantity){
+    alert("Devi selezionare una difficoltà!")
+  }
+  else{
+    for(let i=0; i<cellsQuantity; i++){
+      const cell = createCell(field);
+      cell.addEventListener("click", function(){
+        this.classList.add("clicked");
+      })
+    }
+  }
 }
 
-function getSquaresQuantity(){
+/**
+ * Crea una cella.
+ * @param {HTMLDivElement} target 
+ */
+function createCell(target){
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  const number = getUniqueRandomNumber(1,getCellsQuantity());
+  cell.innerHTML = `<span>${number}</span>`
+  cell.classList.add(getCellSize());
+  target.append(cell);
+  return cell;
+}
+
+/**
+ * Ottiene le dimensioni delle celle in base alla loro quantità.
+ * @returns 
+ */
+function getCellSize(){
+  let size;
+  switch(getCellsQuantity()){
+    case 100:
+      size = "small";
+      break;
+    case 81:
+      size = "medium";
+      break;
+    case 49:
+      size = "big";
+      break;
+    default:
+      size = "";
+      break;
+  }
+  return size;
+}
+
+/**
+ * Ottiene la quantità di celle da stampare.
+ * @returns 
+ */
+function getCellsQuantity(){
   const diffSel = document.getElementById("difficulty").value;
   let quantity;
   switch(diffSel){
@@ -35,6 +88,12 @@ function getSquaresQuantity(){
   return quantity;
 }
 
+/**
+ * Estrae un numero random diverso da quelli estratti in precedenza.
+ * @param {number} min 
+ * @param {number} max 
+ * @returns 
+ */
 function getUniqueRandomNumber(min, max){
   let number = null;
   let valid = false;
